96f2d8917967735f857dca221a9774df0aa15f41
Jericho <mackulinm@gmail.com>
2022-09-21 22:50:27 +0200

    Combat

---
+++ Scripts/Combat/DamageData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Common.Combat
+{
+    [Serializable]
+    public struct DamageData
+    {
+        public GameObject Source;
+        public GameObject Target;
+        public float Damage;
+    }
+}
---
+++ Scripts/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class Singleton<T> : MonoBehaviour where T: Component
+    {
+        [SerializeField] private bool _dontDestroyOnLoad;
+        
+        private T _instance;
+        public T Instance => _instance;
+
+        protected void Awake()
+        {
+            if (_instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = GetComponent<T>();
+            
+            if(_dontDestroyOnLoad)
+                DontDestroyOnLoad(gameObject);
+        }
+    }
+}
