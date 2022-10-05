25163fba5430b211d8e759943654c1723322f5a1
Jericho <mackulinm@gmail.com>
2022-10-05 23:31:29 +0200

    AoE Damage

---
+++ Scripts/Combat/AoeDamage2D.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public static class AoeDamage2D
+    {
+        private static RaycastHit2D[] _results = new RaycastHit2D[10];
+        
+        public static void CreateAOE(DamageData damageData, Vector2 position, float radius, LayerMask _hitLayer)
+        {
+            var hits = Physics2D.CircleCastNonAlloc(position, radius, Vector2.up, _results, radius, _hitLayer);
+            for (int i = 0; i < hits; i++)
+            {
+                var hittable = _results[i].transform.GetComponent<IHittable>();
+                hittable?.Hit(damageData);
+            }
+        }
+    }
+}
