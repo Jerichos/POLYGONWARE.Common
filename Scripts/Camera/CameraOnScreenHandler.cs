da8355920561f0cae42e8a751bb197d6f0cbc81a
Jericho <mackulinm@gmail.com>
2022-10-11 17:20:38 +0200

    OnScreen Triggers using rigidbody collider

--- Scripts/Camera/CameraOnScreenHandler.cs
+++ Scripts/Camera/CameraOnScreenHandler.cs
@@ -9,151 +9,41 @@ namespace Common
     {
         [SerializeField] private Camera _camera;
         [SerializeField] private LayerMask _hitLayer;
-        [SerializeField] float _angle = 0;
-        [SerializeField] private Vector2 _direction = Vector2.right;
-
-        [SerializeField] private Vector2 _size;
-        private int _count;
-
-        private float _distance;
-        private Vector3 _origin;
-
-        private readonly RaycastHit2D[] _results = new RaycastHit2D[25]; //!! Length is the limit of triggers can appear on the screen at one time !!
-        private readonly List<TriggerData> _onScreenTriggers = new();
+        [SerializeField] private Vector2 _sizeOffset = Vector2.one;
+        [SerializeField] private BoxCollider2D _collider;
 
         private void Awake()
         {
+            UpdateColliderSize();
+        }
+
+        private void UpdateColliderSize()
+        {
             var aspect = (float)Screen.currentResolution.width / Screen.currentResolution.height;
             var orthoSize = _camera.orthographicSize;
-            
-            _size.x = 2.0f * orthoSize * aspect;
-            _size.y = 2.0f * _camera.orthographicSize;
 
-            _distance = _size.x;
+            Vector2 size;
+            size.x = 2.0f * orthoSize * aspect;
+            size.y = 2.0f * _camera.orthographicSize;
+
+            _collider.size = size + _sizeOffset;
         }
 
-        // TODO: What about making rigidbody and check OnTriggerEnter/Exit instead?
-        private void TriggerOnScreen()
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            Debug.Log(name + " BoxCastNonAlloc");
-            _origin = _camera.transform.position;
-            _origin.x -= _size.x;
-            _count = Physics2D.BoxCastNonAlloc(_origin, _size, _angle, _direction, _results, _distance, _hitLayer);
-
-            for (int i = 0; i < _count; i++)
-            {
-                var instanceID = _results[i].transform.GetInstanceID();
-                bool add = true;
-
-                for (int j = 0; j < _onScreenTriggers.Count; j++)
-                {
-                    if (_onScreenTriggers[j].ID == instanceID)
-                    {
-                        add = false;
-                        break;
-                    }
-                }
-
-                if (add)
-                {
-                    var trigger = _results[i].transform.GetComponent<IOnScreenTrigger>();
-                    trigger.OnScreenEnter();
-                    TriggerData data = new TriggerData { ID = instanceID, Trigger = trigger};
-                    _onScreenTriggers.Add(data);
-                }
-            }
-
-            for (int i = 0; i < _onScreenTriggers.Count; i++)
-            {
-                bool remove = true;
-                for (int j = 0; j < _count; j++)
-                {
-                    if (_onScreenTriggers[i].ID == _results[j].transform.GetInstanceID())
-                    {
-                        remove = false;
-                        break;
-                    }
-                }
-
-                if (remove)
-                {
-                    _onScreenTriggers[i].Trigger.OnScreenExit();
-                    _onScreenTriggers.RemoveAt(i);
-                }
-            }
+            IOnScreenTrigger trigger = other.GetComponent<IOnScreenTrigger>();
+            trigger?.OnScreenEnter();
         }
 
-        private void OnEnable()
+        private void OnTriggerExit2D(Collider2D other)
         {
-            InvokeRepeating(nameof(TriggerOnScreen), 0, 1);
+            IOnScreenTrigger trigger = other.GetComponent<IOnScreenTrigger>();
+            trigger?.OnScreenExit();
         }
 
-        private void OnDisable()
+        private void OnValidate()
         {
-            _onScreenTriggers.Clear();
-            
-            
-            CancelInvoke();
+            UpdateColliderSize();
         }
-
-        private void OnDrawGizmos()
-        {
-            var origin = (Vector2)_camera.transform.position;
-            origin.x -= _size.x;
-            var aspect = (float)Screen.currentResolution.width / Screen.currentResolution.height;
-            var orthoSize = _camera.orthographicSize;
-            
-            _size.x = 2.0f * orthoSize * aspect;
-            _size.y = 2.0f * _camera.orthographicSize;
-            _distance = _size.x;
-
-            Vector2 p1, p2, p3, p4, p5, p6, p7, p8;
-            float w = _size.x * 0.5f;
-            float h = _size.y * 0.5f;
-            p1 = new Vector2(-w, h);
-            p2 = new Vector2(w, h);
-            p3 = new Vector2(w, -h);
-            p4 = new Vector2(-w, -h);
-
-            Quaternion q = Quaternion.AngleAxis(_angle, new Vector3(0, 0, 1));
-            p1 = q * p1;
-            p2 = q * p2;
-            p3 = q * p3;
-            p4 = q * p4;
-
-            p1 += origin;
-            p2 += origin;
-            p3 += origin;
-            p4 += origin;
-
-            Vector2 realDistance = _direction.normalized * _distance;
-            p5 = p1 + realDistance;
-            p6 = p2 + realDistance;
-            p7 = p3 + realDistance;
-            p8 = p4 + realDistance;
-
-
-            Gizmos.color = Color.magenta;
-            Gizmos.DrawLine(p1, p2);
-            Gizmos.DrawLine(p2, p3);
-            Gizmos.DrawLine(p3, p4);
-            Gizmos.DrawLine(p4, p1);
-
-            Gizmos.DrawLine(p5, p6);
-            Gizmos.DrawLine(p6, p7);
-            Gizmos.DrawLine(p7, p8);
-            Gizmos.DrawLine(p8, p5);
-
-            Gizmos.DrawLine(p1, p5);
-            Gizmos.DrawLine(p2, p6);
-            Gizmos.DrawLine(p3, p7);
-            Gizmos.DrawLine(p4, p8);
-        }
-    }
-
-    public struct TriggerData
-    {
-        public int ID;
-        public IOnScreenTrigger Trigger;
     }
 }
