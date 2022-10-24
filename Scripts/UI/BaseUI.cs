4790e4c13b538feaf181f3e15f2a3de1bdbc5336
Jericho <mackulinm@gmail.com>
2022-10-24 23:06:26 +0200

    Update BaseUI.cs

--- Scripts/UI/BaseUI.cs
+++ Scripts/UI/BaseUI.cs
@@ -1,35 +1,35 @@
 ﻿using System;
+using POLYGONWARE.Common.Util;
 using UnityEngine;
 
 namespace POLYGONWARE.Common.UI
 {
     public abstract class BaseUI : MonoBehaviour, IUI
     {
-        private void Awake()
-        {
-            Close();
-        }
-
         public virtual IUI Open()
         {
+            // gameObject.Log("Open");
             gameObject.SetActive(true);
             return this;
         }
 
         public virtual IUI Open<T>(T arg)
         {
+            // gameObject.Log("Open T");
             gameObject.SetActive(true);
             return this;
         }
 
         public virtual IUI Close()
         {
+            // gameObject.Log("Close");
             gameObject.SetActive(false);
             return this;
         }
 
         public virtual IUI Toggle()
         {
+            // gameObject.Log("toggle");
             var open = !gameObject.activeInHierarchy;
             gameObject.SetActive(open);
             return this;
